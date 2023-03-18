cc480ae65497e0d5aece71fccda6fedbb43f3fd7
matumoto1234 <matumotosizuoka@gmail.com>
2023-03-18 17:40:56 +0900

    modify sample02

--- examples/sample02/main.go
+++ examples/sample02/main.go
@@ -13,49 +13,52 @@ import (
 // 1. Clientのフィールドとして*rate.Limiterを持たせる
 // 2. Do()などの実際にリクエストを送る際に、rate.Limiter.Wait()を呼ぶ
 
-//RateLimitHTTPClient : Rate Limited HTTP Client
+// RateLimitHTTPClient : Rate Limited HTTP Client
 type RateLimitHTTPClient struct {
 	client      *http.Client
 	RateLimiter *rate.Limiter
 }
 
-//Do dispatches the HTTP request to the network
 func (c *RateLimitHTTPClient) Do(req *http.Request) (*http.Response, error) {
-	// Comment out the below 5 lines to turn off ratelimiting
 	ctx := context.Background()
-	err := c.RateLimiter.Wait(ctx) // This is a blocking call. Honors the rate limit
-	if err != nil {
+
+	if err := c.RateLimiter.Wait(ctx); err != nil {
 		return nil, err
 	}
+
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
 	}
+
 	return resp, nil
 }
 
-//NewClient return http client with a ratelimiter
 func NewClient(rl *rate.Limiter) *RateLimitHTTPClient {
 	c := &RateLimitHTTPClient{
 		client:      http.DefaultClient,
 		RateLimiter: rl,
 	}
+
 	return c
 }
 
 func main() {
 	rl := rate.NewLimiter(rate.Every(10*time.Second), 50) // 50 request every 10 seconds
+
 	c := NewClient(rl)
-	reqURL := "http://localhost:4000"
-	req, _ := http.NewRequest("GET", reqURL, nil)
-	for i := 0; i < 300; i++ {
+
+	req, _ := http.NewRequest("GET", "http://localhost:3000", nil)
+
+	for i := 0; i < 100; i++ {
 		resp, err := c.Do(req)
 		if err != nil {
 			fmt.Println(err.Error())
 			fmt.Println(resp.StatusCode)
 			return
 		}
-		if resp.StatusCode == 429 {
+
+		if resp.StatusCode == http.StatusTooManyRequests {
 			fmt.Printf("Rate limit reached after %d requests", i)
 			return
 		}
